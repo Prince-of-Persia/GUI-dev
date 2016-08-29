2deca8e705c4ec37447475fc974fcea0868e4873
Amir <amir_rajabifar@yahoo.com.au>
2016-08-29 14:48:04 +0800

    Layouts

--- 07-HorizontalandVerticalLayouts/main.cpp
+++ 07-HorizontalandVerticalLayouts/main.cpp
@@ -1,11 +1,36 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QWidget *window = new QWidget; //create a window
+    window->setWindowTitle("My App"); //set window title
+
+    QPushButton *button1 = new QPushButton("One");      //create some push buttons
+    QPushButton *button2 = new QPushButton("Two");
+    QPushButton *button3 = new QPushButton("Three");
+
+    QHBoxLayout *hLayout = new QHBoxLayout; // create a horizental layout
+    QVBoxLayout *vLayout = new QVBoxLayout; // create a vertical layout
+
+    /*
+    hLayout->addWidget(button1); //add created push buttons to
+    hLayout->addWidget(button2); //the layut
+    hLayout->addWidget(button3);
+    */
+    vLayout->addWidget(button1); //add created push buttons to
+    vLayout->addWidget(button2); //the layut
+    vLayout->addWidget(button3);
+
+    //to assign the layout to the window
+    //window->setLayout(hLayout); //horizental
+    window->setLayout(vLayout);   //vertical
+
+    window->show();//show the window
 
     return a.exec();
 }
