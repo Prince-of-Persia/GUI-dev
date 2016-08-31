80fcde640717ceb3c3c2fdf43b1fe18daf79769f
Amir <amir_rajabifar@yahoo.com.au>
2016-08-31 11:28:01 +0800

    Qt Radio Button

---
+++ 017-QRadioButton/dialog.cpp
@@ -0,0 +1,30 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+#include <QtCore>
+#include <QtGui>
+#include <QtWidgets>
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+void Dialog::on_pushButton_clicked()
+{
+    if(ui->radioButton1->isChecked())
+    {
+        //QMessageBox::information(this, "Title", "Cats slected");
+        QMessageBox::information(this, "Title", ui->radioButton1->text());
+    }
+    else if(ui->radioButton2->isChecked())
+    {
+        QMessageBox::information(this, "Title", ui->radioButton2->text());
+    }
+}
